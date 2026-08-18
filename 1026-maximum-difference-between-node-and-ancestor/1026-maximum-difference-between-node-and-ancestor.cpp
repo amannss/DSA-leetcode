bf81eef3fc23caa5e2fdfddae9bc137a566d616d
Aman Sharma <amansharma96609@gmail.com>
2026-08-18 18:28:42 +0530

    Time: 0 ms (100%), Space: 13.6 MB (83.97%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int ans = -1e9 ;
+    void solve(TreeNode* root , int maxi , int mini)
+    {
+        if(!root) return ;
+        ans = max(max(abs(root->val - maxi) , abs(root->val - mini)),ans) ;
+        maxi = max(maxi , root->val) ;
+        mini = min(mini , root->val) ;
+        solve(root->left ,maxi , mini ) ;
+        solve(root->right , maxi , mini ) ;
+        return ;
+    }
+    int maxAncestorDiff(TreeNode* root) {
+        if(!root) return 0 ;
+        solve(root , root->val,root->val) ;
+        return ans ;
+    }
+};
