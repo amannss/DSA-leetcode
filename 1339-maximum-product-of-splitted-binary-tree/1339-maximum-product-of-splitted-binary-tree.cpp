a1ffc580b8fa281b41b207124d686188792eec5e
Aman Sharma <amansharma96609@gmail.com>
2026-08-18 20:09:32 +0530

    Time: 7 ms (38.04%), Space: 91.5 MB (97.58%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,30 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    long long totalSum = 0;
-    long long best = 0;
-    const int MOD = 1e9 + 7;
-
-    long long getSum(TreeNode* root) {
-        if (!root) return 0;
-        return root->val + getSum(root->left) + getSum(root->right);
+long long sum = 0 ;
+long long best =  0 ;
+int mod = 1e9 + 7 ;
+    long long  totalsum(TreeNode* root)
+    {
+        if(!root) return 0 ;
+        return root->val + totalsum(root->left ) + totalsum(root->right) ;
     }
+    long long  solve(TreeNode* root)
+    {
+        if(!root) return 0 ;
+        long long ls = solve(root->left ) ;
+        long long rs = solve(root->right) ;
+        long long s1 = root->val + ls + rs ;
+        long long s2 = sum  -s1 ;
+        best = max(best , s1 * s2 ) ;
+        return root->val + ls + rs ;
 
-    long long dfs(TreeNode* root) {
-        if (!root) return 0;
-
-        long long left = dfs(root->left);
-        long long right = dfs(root->right);
-
-        long long subSum = root->val + left + right;
-
-        best = max(best, subSum * (totalSum - subSum));
-
-        return subSum;
     }
-
     int maxProduct(TreeNode* root) {
-        totalSum = getSum(root);   // Step 1: total tree sum
-        dfs(root);                 // Step 2: try all splits
-        return best % MOD;         // Step 3: modulo at the end
+        sum = totalsum(root) ;
+        solve(root) ;
+        return best % mod ;
     }
 };
